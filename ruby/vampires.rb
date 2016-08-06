69efbf4ab7afb389017bf2c0503bf56074e88047
Corbin_Via <corbinvia@gmail.com>
2016-08-05 20:08:35 -0500

    Create method for vampire screening (Release 1)

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+def vampire_test
+	puts "Welcome to Werewolf Inc. What is your name?"
+	name = gets.chomp
+	puts "How old are you? What year were you born?"
+	age = gets.to_i
+	dob = gets.to_i
+	puts = "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic = gets.chomp
+	puts "Would you like to enroll in the company's health insurance?"
+	insurance = gets.chomp
+end
+
