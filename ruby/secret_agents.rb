adee9385288921e19a985768fa412ad34f439e22
Corbin_Via <corbinvia@gmail.com>
2016-08-07 16:35:26 -0500

    Add condition for z

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -7,10 +7,28 @@
 def encrypt(str)
    index = 0
    while index < str.length
+      if str[index] = "z"
+       	str[index] = "a"
+       	break
+       end
        str[index] = str[index].next 
        index += 1
    end
    p str
 end
 
-encrypt("abc")
+def decrypt(str)
+	index = 0
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	while index < str.length
+       num = alphabet.index(str[index]) 
+       str[index] = alphabet[num-1]
+       index += 1
+   end
+   p str
+end
+
+encrypt("abc") #should return "bcd"
+encrypt("zed") #should return "afe"
+decrypt("bcd") #should return "abc"
+decrypt("afe") #should return "zed"
