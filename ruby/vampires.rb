4185bd31d773c8ed1733073cc86aa650d7225e51
Corbin_Via <corbinvia@gmail.com>
2016-08-07 17:40:36 -0500

    Add current year variable

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,16 +2,17 @@ puts "How many new employees will be processed today?"
 number_of_employees = gets.to_i
 current_count = 0
 allergies = nil
+current_year = 2016
 while current_count < number_of_employees do
 	puts "Welcome to Werewolf Inc. What is your name?"
-		name = gets.chomp
-		puts "How old are you? What year were you born?"
-		age = gets.to_i
-		dob = gets.to_i
-		puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-		garlic = gets.chomp.downcase
-		puts "Would you like to enroll in the company's health insurance?"
-		insurance = gets.chomp.downcase
+	name = gets.chomp
+	puts "How old are you? What year were you born?"
+	age = gets.to_i
+	dob = gets.to_i
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic = gets.chomp.downcase
+	puts "Would you like to enroll in the company's health insurance?"
+	insurance = gets.chomp.downcase
 	loop do
 		puts "Do you have any allergies?"
 		allergies = gets.chomp.downcase
@@ -24,41 +25,41 @@ while current_count < number_of_employees do
 		break
 		end
 	end
-		if age == (2016-dob)
-			age = false
-		else
-			age = true
-		end
-		if garlic.include? "no"
-			garlic = true
-		else
-			garlic = false
-		end
-		if insurance.include? "no"
-			insurance = true
-		else
-			insurance = false
-		end
-		if name.include? "Drake Cula"
-			name = true
-		elsif name.include? "Tu Fang"
-			name = true
-		else
-			name = false
-		end
-		if name == true
-			puts "Definitely a vampire."
-		elsif allergies == true
-			puts "Probably a vampire."
-		elsif (age == true) && (garlic == true) && (insurance == true)
-			puts "Almost certainly a vampire."
-		elsif (age == true) && ((garlic == true) || (insurance == true))
-			puts "Probably a vampire."
-		elsif (age != true) && (garlic != true) && (insurance != true)
-			puts "Probably not a vampire."
-		else
-			puts "Results inconclusive."
-		end
-		current_count += 1
+	if age == (current_year - dob)
+		age = false
+	else
+		age = true
 	end
-		puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+	if garlic.include? "no"
+		garlic = true
+	else
+		garlic = false
+	end
+	if insurance.include? "no"
+		insurance = true
+	else
+		insurance = false
+	end
+	if name.include? "Drake Cula"
+		name = true
+		elsif name.include? "Tu Fang"
+		name = true
+	else
+		name = false
+	end
+	if name == true
+		puts "Definitely a vampire."
+		elsif allergies == true
+		puts "Probably a vampire."
+		elsif (age == true) && (garlic == true) && (insurance == true)
+		puts "Almost certainly a vampire."
+		elsif (age == true) && ((garlic == true) || (insurance == true))
+		puts "Probably a vampire."
+		elsif (age != true) && (garlic != true) && (insurance != true)
+		puts "Probably not a vampire."
+	else
+		puts "Results inconclusive."
+	end
+	current_count += 1
+end
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
