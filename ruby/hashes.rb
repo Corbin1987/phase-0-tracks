2cccd399a5aad31ffa0a84106bfd754e95052a64
Corbin_Via <corbinvia@gmail.com>
2016-08-13 14:28:32 -0500

    Add pseudocode and fix if statement

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -18,17 +18,7 @@
 # be sure to pseudocode, and leave your pseudocode in as comments.
 
 #Create hash for interior design client
-client = {
-		# name: "", 
-		# age: "", 
-		# number_of_rooms: "", 
-		# children?: "", 
-		# decor_theme: "", 
-		# favorite_color: "", 
-		# furniture_in_room?: "", 
-		# room_lighting: "",
-		# pets?: ""
-	}
+client = {}
 #Welcome message
 puts "Welcome interior designer! Please input information about your new client. For any output with a question mark, please enter true/false."
 #Prompt user for information
@@ -68,9 +58,45 @@ puts "Pets?"
 pets = gets.chomp
 client[:pets?] = pets
 #Ask for any edits
-puts "Would you like to edit any of this information? Please enter yes/no."
+puts "Would you like to edit any of this information? Please enter a key to edit or enter 'no' if finished."
 user_input = gets.chomp
 if user_input == "no"
-	p client
-elsif user_input == "yes"
+elsif user_input == "name"
+	puts "Name:"
+	name = gets.chomp
+	client[:name] = name
+elsif user_input == "age"
+	puts "Age:"
+	age = gets.chomp
+	client[:age] = age
+elsif user_input == "number of rooms"
+	puts "Number of rooms:"
+	rooms = gets.chomp
+	client[:number_of_rooms] = rooms
+elsif user_input == "children"
+	puts "Children?"
+	children = gets.chomp
+	client[:children?] = children
+elsif user_input == "decor theme"
+	puts "Decor theme:"
+	decor = gets.chomp
+	client[:decor_theme] = decor
+elsif user_input == "favorite color"
+	puts "Favorite color:"
+	color = gets.chomp
+	client[:favorite_color] = color
+elsif user_input == "furniture in room"
+	puts "Furniture in room?"
+	furniture = gets.chomp
+	client[:furniture_in_room?] = furniture
+elsif user_input == "room lighting"
+	puts "Room lighting:"
+	lighting = gets.chomp
+	client[:room_lighting] = lighting
+elsif user_input == "pets"
+	puts "Pets?"
+	pets = gets.chomp
+	client[:pets?] = pets
 end
+#Print hash information to user
+p client
