2c93f93255422e1eb43946103007e3f11784e04b
Corbin_Via <corbinvia@gmail.com>
2016-08-13 15:50:11 -0500

    Add commas and fix indentation after checking in REPL

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,12 +1,86 @@
 vehicle = {
-	engine:
-	interior:
-	exterior:
-	suspension:
-	heating:
-	cooling:
-	brakes:
-	transmission:
-	exhaust:
-	fuel_system:
+	engine: [
+		{engine_type: [
+			"inline-four",
+			"V6",
+			"V8",
+			"V10",
+			"V12"]
+		},
+		"engine block",
+		"pistons",
+		"valves",
+		"spark plugs",
+		"timing chain/belt",
+		"crankshaft",
+		"camshaft"
+	],
+	interior: [
+		"seats", 
+		"seatbelts", 
+		"floor mats", 
+		"cargo mats"
+		],
+	exterior: [
+		"mirrors", 
+		"headlights", 
+		"brake lights", 
+		"windshield wipers"
+		],
+	suspension: [
+		{suspension_type: [
+			"struts", 
+			"shocks", 
+			"leaf springs"]
+		},
+		"stabilizer bar",
+		"stablizer links",
+		"strut mounts",
+		"control arms",
+		"tie rods" 
+		],
+	heating: [
+		"heater core",
+		"hoses",
+		"blower motor"
+	],
+	cooling: [
+		"compressor",
+		"condenser",
+		"receiver/drier",
+		"hoses",
+		"blower motor",
+		"evaporator"
+	],
+	brakes: [
+		"rotors",
+		"drums",
+		"brake pads",
+		"brake shoes",
+		"parking brake cable",
+		"calipers",
+		"brake hoses"
+	],
+	transmission: [
+		{transmission_type: [
+			"automatic",
+			"manual",
+			"CVT",
+			"semi-automatic"]
+		},
+		"axles",
+		"cv joints",
+		"differential",
+		"clutch"
+		],
+	exhaust: [
+		"oxygen sensors",
+		"catalytic converter",
+		"muffler"
+	],
+	fuel_system: [
+		"fuel pump",
+		"fuel injector",
+		"fuel tank"
+	]
 }
