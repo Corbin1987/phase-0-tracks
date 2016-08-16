0afa77cb352d9bcce6c5c78197c7a8aa1ccbadf9
Corbin_Via <corbinvia@gmail.com>
2016-08-16 05:30:50 -0500

    Add puppy attributes and methods

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+How might you design a Puppy class? What sorts of characteristics and behavior might a Puppy class involve? 
+Using a format similar to the one we used for Eyelashus Adorableus, come up with some characteristics and 
+behavior for a Puppy class. 
+Use comments to add your class design to phase-0-tracks/ruby/puppy.rb.
+
+# Puppy class! (exclamation point added to enhance cuteness)
+# Class design would include: favorite names, famous puppies, puppy abilities (running, tail-wagging, excited
+# wiggling, rolling over, making unexpected puddles).
+# Class design would also include: other puppy abilities such as fetching, sitting, staying, and rolling over 
+# as inherent puppy methods.
+# Class design might also include puppy attributes such as: four legs, a tail, adorable eyes to which you can't
+# say no, and a beautiful puppy smile.
