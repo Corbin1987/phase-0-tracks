a6f6be0659021ee31eb983ccf7724d5b9f12bcd4
Corbin_Via <corbinvia@gmail.com>
2016-08-20 21:00:40 -0500

    Add questions for initializing and armor to user interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,18 +1,5 @@
-# at least three attributes (using at least two data types) whose values will vary from instance to instance (example: age)
-# at least three methods, at least one of which takes an argument
-# Pseudocode your design as comments in phase-0-tracks/ruby/solo.rb.
-# In the same file as your design, implement your class. When you're not sure how to implement a behavior 
-# (how the heck do we make a dragon breathe fire?), you might consider a simple printout of what's going on. 
-
-# Your class should ...
-
-# Use attr_reader and attr_accessor
-# Use an initialize method that sets at least one attribute
-# Feel free to tweak your original design if needed to make this possible.
-
 # Modify your program so that it has a user interface 
 # (a user can interact with your program via the command line).
-
 # Your user should be allowed to create as many instances of your class as they like.
 # Prompt the user for each attribute, and don't forget to convert their input to the appropriate data type. 
 # Store these class instances in an array.
@@ -28,22 +15,27 @@
 # Armor: rating 1-10
 # Weapons: any value (strings)
 # Skills: any value (strings)
-# Methods: change armor rating, add/delete weapons, add/delete skills
+# Methods: change armor rating, add/delete weapons, add/delete skills, engage in battle
 # Armor rating will be set at default value of 1
 # Add/delete weapons will take an argument
 # Maximum amount of weapons will be three
 # Add/delete skills will take an argument
 # Maximum amount of skills will be five
+# Engage in battle will reset location to where warrior is fighting
 
 class Warrior
 	attr_reader :location
 	attr_accessor :armor, :weapons, :skills
 
-	def initialize(armor, weapons, skills)
-		@armor = 1
+	def initialize #(armor=1, weapons, skills)
+		@armor = armor
 		@weapons = []
 		@skills = []
 	end
+
+	# def change_armor(number)
+	# 	@armor = number
+	# end
 
 	def add_weapon(weapon)
 		@weapons << weapon
@@ -52,4 +44,37 @@ class Warrior
 
 	def delete_weapon(weapon)
 		@weapons.delete(weapon)
+		@weapons
+	end
+
+	def add_skill(skill)
+		@skills << skill
+		@skills
+	end
+
+	def delete_skill(skill)
+		@skills.delete(skill)
+		@skills
+	end
+
+	def engage(location)
+		@location = location
+		p "Your warrior has engaged the enemy at #{location}."
+	end
+end
+
+puts "Welcome to Warrior Creator. Please enter a name for your warrior."
+name = gets.chomp
+name = Warrior.new
+
+puts "Set an armor rating for your warrior."
+armor_rating = gets.chomp
+
+if armor_rating <= 10
+	name.armor=(armor_rating)
+	puts "Your warrior has an armor rating of #{@armor}."
+elsif armor_rating >=11
+	puts "Sorry, you can't have armor that heavy."
+else
+	puts "Invalid input. Please enter a number from 1 to 10."
 end
