a4d18e7ff8164fe924950c288c5e5c35e475f0f7
Corbin_Via <corbinvia@gmail.com>
2016-08-21 08:18:13 -0500

    Add different methods, fix pseudocoding

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,91 +1,78 @@
-# Modify your program so that it has a user interface 
-# (a user can interact with your program via the command line).
-# Your user should be allowed to create as many instances of your class as they like.
-# Prompt the user for each attribute, and don't forget to convert their input to the appropriate data type. 
-# Store these class instances in an array.
-# When the user indicates that they are finished creating instances, 
-# loop through the array and print out the attributes of each instance as a confirmation message of what was created.
-
-
 # Pseudocode
 
 # Class will be a warrior creator
-# Attributes: location, armor, weapons, skills
+# Attributes: location, name, armor, weapon
 # Location: read-only attribute
-# Armor: rating 1-10
+# Armor: numerical rating
 # Weapons: any value (strings)
 # Skills: any value (strings)
-# Methods: change armor rating, add/delete weapons, add/delete skills, engage in battle
-# Armor rating will be set at default value of 1
-# Add/delete weapons will take an argument
-# Maximum amount of weapons will be three
-# Add/delete skills will take an argument
-# Maximum amount of skills will be five
+# Methods: engage in battle, attack, defend
 # Engage in battle will reset location to where warrior is fighting
+# Attack will take argument of weapon to use
+# Defend will print message to user
 
 class Warrior
 	attr_reader :location
-	attr_accessor :name, :armor, :weapons
+	attr_accessor :name, :armor, :weapon
 
 	def initialize(name)
 		@name = name
 		@armor = armor
-		@weapons = weapons
+		@weapon = weapon
 	end
-
-	# def change_armor(number)
-	# 	@armor = number
-	# end
-
-	def add_weapon(weapon)
-		@weapons << weapon
-		@weapons
-	end
-
-	def delete_weapon(weapon)
-		@weapons.delete(weapon)
-		@weapons
-	end
-
+	
 	def engage(location)
 		@location = location
-		p "Your warrior has engaged the enemy at #{location}."
+		p "Your warrior has engaged the enemy in #{location}."
+	end
+	
+	def attack(weapon)
+		p "Your warrior attacked the enemy with a warrior #{weapon}!"
+	end
+	
+	def defend
+		p "Your warrior has defended the enemy's attack."
 	end
 end
 
 puts "Welcome to Warrior Creator."
 entries = []
 user_info = {}
+
 loop do
 puts "Please enter a name for your warrior."
 user_name = gets.chomp
-user_info[:Name] = "#{user_name}"
+user_info["Name"] = "#{user_name}"
 user_name = Warrior.new(user_name)
 
 puts "Set an armor rating for your warrior."
 armor_rating = gets.to_i
-user_info[:Armor] = "#{armor_rating}"
-if armor_rating <= 10
-	user_name.armor=(armor_rating)
-	puts "Your warrior has an armor rating of #{user_name.armor}."
-elsif armor_rating >= 11
-	puts "Sorry, you can't have armor that heavy."
-else armor_rating == String
-	puts "Invalid input. Please enter a number from 1 to 10."
-end
+user_info["Armor"] = "#{armor_rating}"
+user_name.armor=(armor_rating)
+puts "Your warrior has an armor rating of #{user_name.armor}."
 
-puts "Add weapons for your warrior."
-user_weapons = gets.chomp
-user_info[:Weapons] = "#{user_weapons}"
-user_weapons.split(' ')
-user_name.weapons=(user_weapons)
+puts "Add a weapon for your warrior."
+user_weapon = gets.chomp
+user_info["Weapon"] = "#{user_weapon}"
+user_name.weapon=(user_weapon)
+p "Your warrior wields the #{user_name.weapon}."
 
 
 puts "Where would you like to engage your warrior?"
 user_location = gets.chomp
-user_info[:Location] = "#{user_location}"
+user_info["Location"] = "#{user_location}"
 user_name.engage(user_location)
+
+puts "Attack the enemy?"
+user_attack = gets.chomp.downcase
+if user_attack == "no"
+	user_name.defend
+else
+	user_name.attack(user_weapon)
+end
+
 entries.push(user_info)
+
 puts "Would you like to create another warrior?"
 create_new = gets.chomp.downcase
 break if create_new == "no"
