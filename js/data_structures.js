49b12c8a2a77c5f9184afa46698bb1cccad485a6
Corbin_Via <corbinvia@gmail.com>
2016-08-28 13:19:43 -0500

    Add colorsArray and namesArray

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,1 +1,20 @@
 // Corbin Via: DBC Week 7.2 Data Structures
+
+//In your new file, declare two separate arrays:
+// an array of four colors ("blue", for instance, though you're welcome to get crazy with something like "iridescent blue/green with silver sparkles")
+// an array of four names that you might give a horse ("Ed" is always a solid choice to start with)
+// Write additional code that adds another color to the colors array, and another horse name to the horses array.
+
+// Release 0
+
+var colorsArray = ["blue", "red", "green", "yellow"];
+
+var namesArray = ["Mr. Ed", "Flash Lightning", "Corbin", "Marvan"];
+
+colorsArray.push("purple");
+
+console.log(colorsArray);
+
+namesArray.push("Ed Junior");
+
+console.log(namesArray);
