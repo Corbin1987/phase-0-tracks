0aea4e0cc2c7685d9dc6f45591fafe7ba85bc229
Corbin_Via <corbinvia@gmail.com>
2016-09-04 21:50:10 -0500

    Add pseudocode

--- week_8.5/rubysql_test_program.rb
+++ week_8.5/rubysql_test_program.rb
@@ -1,13 +1,17 @@
 # Corbin Via: DBC Week 8.5 Assignment: Combining SQL and Ruby
 
+# BUSINESS LOGIC
+
 # Use SQLite3 to store data
 require 'sqlite3'
 
-puts "Welcome to Grocery List Maker.\n Please enter a name for your data:"
-user_input = gets.chomp
-db = SQLite3::Database.new("#{user_input}.db")
-db.results_as_hash = true
+# Method to create an entry in table
+def create_entry(db, item, amount)
+   db.execute("INSERT INTO list (item, amount) VALUES (?, ?)", [item, amount])
+end
 
+# Create table method
+# Table called list will be created with values of item and amount
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS list(
     id INTEGER PRIMARY KEY,
@@ -16,22 +20,39 @@ create_table_cmd = <<-SQL
   )
 SQL
 
+# USER INTERFACE
+
+# Greeting message
+puts "Welcome to Grocery List Maker.\nPlease enter a name for your data:"
+# Collect user input
+user_input = gets.chomp
+# User input is stored as new database name
+db = SQLite3::Database.new("#{user_input}.db")
+# Database results are stored as a hash
+db.results_as_hash = true
+# Create table method is called by execute method
 db.execute(create_table_cmd)
-
-def create_entry(db, item, amount)
-   db.execute("INSERT INTO list (item, amount) VALUES (?, ?)", [item, amount])
- end
-
-
+# Ask user for amount of items
 puts "How many items would you like to enter into this list?"
-amount_of_items = gets.to_i
-
+# Collect user input
+amount_of_items = gets.chomp.to_i
+# Declare count variable
 count = 0
+# Use while loop set to less than user input of amount of items
 while count < amount_of_items
+	# Ask user for item
 	puts "Please enter an item."
+	# Collect user input
 	user_item = gets.chomp
+	# Ask user for amount
 	puts "Please enter an amount."
+	# Collect user input
 	user_amount = gets.chomp.to_i
+	# Use create entry method with variables of db and user inputs
 	create_entry(db, user_item, user_amount)
+	# Increase counter and repeat loop
 	count += 1
 end
+
+puts "Would you like to update an item or delete an item from this list?"
+user_update = gets.chomp
