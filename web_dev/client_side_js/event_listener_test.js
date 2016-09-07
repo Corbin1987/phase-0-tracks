b4da746f667e01aadefc2447b137bd3e9b037010
Corbin_Via <corbinvia@gmail.com>
2016-09-06 22:59:46 -0500

    Add border function to all headers on index.html

--- web_dev/client_side_js/event_listener_test.js
+++ web_dev/client_side_js/event_listener_test.js
@@ -2,18 +2,25 @@
 
 // Event Listeners
 
-console.log("This script is going to edit some things on the site.")
+console.log("This script is going to edit some things on the site.");
 
 function addBorder(event){
 	event.target.style.border = "5px solid black";
 }
 
 function takeAwayBorder(event){
-	event.target.style.border = "none"
+	event.target.style.border = "none";
 }
 
-var testElement = document.getElementById("main-page-header");
+var mainPageHeader = document.getElementById("main-page-header");
+var mainPageHeader2 = document.getElementById("main-page-header-2");
+var mainPageHeader3 = document.getElementById("main-page-header-3");
 
-testElement.addEventListener("mouseenter",addBorder)
+mainPageHeader.addEventListener("click",addBorder);
+mainPageHeader.addEventListener("dblclick",takeAwayBorder);
 
-testElement.addEventListener("mouseleave",takeAwayBorder)
+mainPageHeader2.addEventListener("click",addBorder);
+mainPageHeader2.addEventListener("dblclick",takeAwayBorder);
+
+mainPageHeader3.addEventListener("click",addBorder);
+mainPageHeader3.addEventListener("dblclick",takeAwayBorder);
