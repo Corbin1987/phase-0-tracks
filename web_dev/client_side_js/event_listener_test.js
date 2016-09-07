ad54eab747a7d0ddc32cc95016bc0f185de285ae
Corbin_Via <corbinvia@gmail.com>
2016-09-07 11:55:52 -0500

    Edit event_listener_test, add comments

--- web_dev/client_side_js/event_listener_test.js
+++ web_dev/client_side_js/event_listener_test.js
@@ -2,8 +2,29 @@
 
 // Event Listeners
 
+// Message to be printed to the console
 console.log("This script is going to edit some things on the site.");
 
+// Variable declarations
+var body = document.getElementById("main-page");
+
+var mainPageHeader = document.getElementById("main-page-header");
+
+var mainPageHeader2 = document.getElementById("main-page-header-2");
+
+var mainPageHeader3 = document.getElementById("main-page-header-3");
+
+var orangeButton = document.getElementById("orange-button");
+
+var redButton = document.getElementById("red-button");
+
+var whiteButton = document.getElementById("white-button");
+
+var purpleButton = document.getElementById("purple-button");
+
+var greenButton = document.getElementById("green-button");
+
+// Function declarations
 function addBorder(event) {
 	event.target.style.border = "5px solid black";
 }
@@ -13,50 +34,44 @@ function takeAwayBorder(event) {
 }
 
 function changeColorToRed() {
-	var body = document.getElementById("main-page");
 	body.style.background = "red";
 }
 
 function changeColorToWhite() {
-	var body = document.getElementById("main-page");
 	body.style.background = "white";
 }
 
 function changeColorToOrange() {
-	var body = document.getElementById("main-page");
 	body.style.background = "orange";
 }
 
 function changeColorToPurple() {
-	var body = document.getElementById("main-page");
 	body.style.background = "purple";
 }
 
 function changeColorToGreen() {
-	var body = document.getElementById("main-page");
 	body.style.background = "green";
 }
 
-var mainPageHeader = document.getElementById("main-page-header");
-var mainPageHeader2 = document.getElementById("main-page-header-2");
-var mainPageHeader3 = document.getElementById("main-page-header-3");
-var orangeButton = document.getElementById("orange-button");
-var redButton = document.getElementById("red-button");
-var whiteButton = document.getElementById("white-button");
-var purpleButton = document.getElementById("purple-button");
-var greenButton = document.getElementById("green-button");
-
+// Event listener declarations
 mainPageHeader.addEventListener("click", addBorder);
+
 mainPageHeader.addEventListener("dblclick", takeAwayBorder);
 
 mainPageHeader2.addEventListener("click", addBorder);
+
 mainPageHeader2.addEventListener("dblclick", takeAwayBorder);
 
 mainPageHeader3.addEventListener("click", addBorder);
+
 mainPageHeader3.addEventListener("dblclick", takeAwayBorder);
 
 redButton.addEventListener("click", changeColorToRed);
+
 whiteButton.addEventListener("click", changeColorToWhite);
+
 orangeButton.addEventListener("click", changeColorToOrange);
+
 purpleButton.addEventListener("click", changeColorToPurple);
+
 greenButton.addEventListener("click", changeColorToGreen);
