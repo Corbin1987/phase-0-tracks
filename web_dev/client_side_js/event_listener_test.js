543ff802d6c9bb12b4cf7af13fab0c0899225772
Corbin_Via <corbinvia@gmail.com>
2016-09-07 10:25:31 -0500

    Add buttons to change background color

--- web_dev/client_side_js/event_listener_test.js
+++ web_dev/client_side_js/event_listener_test.js
@@ -4,23 +4,47 @@
 
 console.log("This script is going to edit some things on the site.");
 
-function addBorder(event){
+function addBorder(event) {
 	event.target.style.border = "5px solid black";
 }
 
-function takeAwayBorder(event){
+function takeAwayBorder(event) {
 	event.target.style.border = "none";
+}
+
+function changeColorToRed() {
+	var body = document.getElementById("main-page");
+	body.style.background = "red";
+}
+
+function changeColorToWhite() {
+	var body = document.getElementById("main-page");
+	body.style.background = "white";
+}
+
+function changeColorToOrange() {
+	var body = document.getElementById("main-page");
+	body.style.background = "orange";
 }
 
 var mainPageHeader = document.getElementById("main-page-header");
 var mainPageHeader2 = document.getElementById("main-page-header-2");
 var mainPageHeader3 = document.getElementById("main-page-header-3");
 
-mainPageHeader.addEventListener("click",addBorder);
-mainPageHeader.addEventListener("dblclick",takeAwayBorder);
+mainPageHeader.addEventListener("click", addBorder);
+mainPageHeader.addEventListener("dblclick", takeAwayBorder);
 
-mainPageHeader2.addEventListener("click",addBorder);
-mainPageHeader2.addEventListener("dblclick",takeAwayBorder);
+mainPageHeader2.addEventListener("click", addBorder);
+mainPageHeader2.addEventListener("dblclick", takeAwayBorder);
 
-mainPageHeader3.addEventListener("click",addBorder);
-mainPageHeader3.addEventListener("dblclick",takeAwayBorder);
+mainPageHeader3.addEventListener("click", addBorder);
+mainPageHeader3.addEventListener("dblclick", takeAwayBorder);
+
+var redButton = document.getElementById("red-button");
+redButton.addEventListener("click", changeColorToRed);
+
+var whiteButton = document.getElementById("white-button");
+whiteButton.addEventListener("click", changeColorToWhite);
+
+var orangeButton = document.getElementById("orange-button");
+orangeButton.addEventListener("click", changeColorToOrange);
