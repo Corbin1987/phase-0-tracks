da0deaf483c3b61b762ba7ab486adb0796f8b21d
Corbin_Via <corbinvia@gmail.com>
2016-09-09 10:17:05 -0500

    Add slideToggle function to unordered list

--- web_dev/jquery/jQueryTest.js
+++ web_dev/jquery/jQueryTest.js
@@ -1,13 +1,14 @@
-$(document).ready(function(){
-    $("#main-page-header").mouseenter(function(){
-        $(this).fadeOut(4000);
-    });
-});
-$(document).ready(function(){
-    $("#main-page-header").mouseleave(function(){
-        $(this).fadeIn(4000);
-    });
-});
+// $(document).ready(function(){
+//     $("#main-page-header").mouseenter(function(){
+//         $(this).fadeOut(4000);
+//     });
+// });
+// $(document).ready(function(){
+//     $("#main-page-header").mouseleave(function(){
+//         $(this).fadeIn(4000);
+//     });
+// });
+
 $(document).ready(function(){
     $("span").on({
         mouseenter: function(){
@@ -28,4 +29,10 @@ $(function(){
 			$(this).css({"height": "100px", "width": "150px"});
 		}
 	});
+});
+
+$(function(){
+    $(".elements").click(function(){
+        $("ul").slideToggle("slow");
+    });
 });
